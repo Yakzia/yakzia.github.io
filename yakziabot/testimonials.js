ebfaaedb4ac08df472ee1dcbc4ac76d2fd834157
Jonathan Mendoza <yakzia@live.com>
2021-05-30 09:08:21 -0500

    v 0.0.30
    Página para el YakziaBot.

---
+++ yakziabot/testimonials.js
@@ -0,0 +1,28 @@
+var listOfTestimonials = {
+    "testimonials": [
+        {
+            "author": "El Killa Caef",
+            "squad": "Clan TKILLA",
+            "image": "ElKillaCaef.png",
+            "testimonial": "Durante mi tiempo usando el YakziaBot he podido agregar a varios jugadores con los que llegue a competir en el multijugador, ya sea en partidas uno contra uno así como en partidas con algunos compañeros.<br><br>Me ha servido bastante para poder ver en cuanto tiempo y en cuantas partidas he podido subir mi nivel así como también saber cuanto he bajado, pues hay ocasiones en las que salgo del juego y no revise mis datos antes de salir.<br><br>Con el YakziaBot pude aprender mejor en que mapas juego mejor, en que mapas puedo mejorar y sobretodo, conocí a un compañero y excelente maestro.<br><br><mark>La única desventaja que le veo, es que no lo tengan los demás canales de discord o algunas páginas del Age</mark><br><br>Es una verdadera maravilla y le agradezco bastante a su creador Yakzia."
+        },
+        {
+            "author": "[LGe]TheNexusEmpire",
+            "squad": "The Nexus Empire",
+            "image": "TheNexusEmpire.png",
+            "testimonial": "Ha sido un excelente bot, super sencillo de utilizar y super completo en todo sentido, <mark>para encontrar partidas que se estan jugando me es super util para los streams</mark>.<br><br>Amo el bot."
+        },
+        {
+            "author": "LautiPaluci",
+            "squad": "LatiNoobs-AoE",
+            "image": "LautiPaluci.gif",
+            "testimonial": "Estoy en más de 30 discords del Age Of Empires y en ninguno vi un bot tan bueno. Ya que hay muchos bots que se encuentran únicamente en inglés o no son muy fáciles de usar. <mark>Creo que este bot es de los mejores, manejo rápido y sencillo, una interfaz muy buena y los datos son exactos.</mark> Sin dudas se los recomiendo."
+        },
+        {
+            "author": "Banztenak_19",
+            "squad": "Clan KMD",
+            "image": "Banztenak_19.gif",
+            "testimonial": "Este bot no sólo te dice el elo de los jugadores, te detalla la partida que se esta jugando o en su defecto que se jugó. Te da la id para observar la partida bajo un click, te muestra los colores de los jugadores o sea su posición en el mapa junto con su civilización (esa civilización también tiene un link para que veas su arbol de tecnologías y puedas aprender más sobre esa civi wtf!). Además te da una forma sencilla para entrar a las salas no puntuadas; normalmente te demoras en entrar puesto que no encuentras la partida o no te pueden invitar (gracias DE), esta función es maravillosa y facilita el momento de entrar ya sea a una partida de torneo o a una pachanga con amigos. Podría escribir un ensayo sobre las funciones del bot y por qué pienso que son un aporte maravilloso para nosotros los jugadores. Creo que todo lo puedo resumir como <mark>un Bot increíble con mucho potencial y que va a seguir sorprendiendome y a la comunidad con su desarrollo.</mark>"
+        }
+    ]
+}
